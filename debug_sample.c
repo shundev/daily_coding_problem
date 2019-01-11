c713b375fa796270d0dc31d9b81d3bc2499f59e3
hondo-s <shunsuke.hondo@gmail.com>
2019-01-11 16:04:53 +0900

    Setup C debug environment

---
+++ debug_sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Steps to debug on VSCode
+
+1. Focus on C file
+2. Set breakpoints
+3. Press F5
+
+*/
+
+int main ()
+{
+    int a = 0, b = 10;
+    int c = a + b;
+    printf("hello %d\n", c);
+}
